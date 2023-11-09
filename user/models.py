0b7513d0f8653bce625b822e0f79b043a42a68f8
rubychaudhary0 <rubychaudhary743@gmail.com>
2023-11-09 19:38:45 +0545

    added user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+from user.manager import AccountManager
 
 # Create your models here.
+class Account(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=150)
+    phone = models.CharField(max_length=50)
+    date_of_birth = models.DateField(blank=True, null=True)
+    picture = models.ImageField(blank=True, null=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    last_login = models.DateTimeField(null=True)
+
+    objects = AccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'phone']
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name.split()[0]
